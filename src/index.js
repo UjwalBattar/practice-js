39ab302ac8f61f3bf27a6ed30ea633f9fa8c6ab9
Ujwal Battar <ujwal.battar@gmail.com>
2018-12-18 12:34:42 -0800

    return and complete queue

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+// import Promise from 'promise';
+import {
+    resolve
+} from 'path';
+import {
+    rejects
+} from 'assert';
+
 document.addEventListener('click', (e) => {
-	// console.log(e.target.tagName === 'DIV');
-	if (e.target.tagName === 'DIV') {
-		alert(`Hi! I am: ${e.target.id}`);
-	}
+    // console.log(e.target.tagName === 'DIV');
+    if (e.target.tagName === 'DIV') {
+        alert(`Hi! I am: ${e.target.id}`);
+    }
 });
 
 let _loading = false;
@@ -11,54 +19,51 @@ const loadingQueue = [];
 let progressBarContainer;
 
 function loadProgressBarsButton() {
-	const loadButton = document.createElement('BUTTON');
-	progressBarContainer.appendChild(loadButton);
-	loadButton.id = 'click-me';
-	loadButton.innerHTML = 'click me!';
-	loadButton.addEventListener('click', () => {
-		addBarToQueue(loading);
-	});
+    const loadButton = document.createElement('BUTTON');
+    progressBarContainer.appendChild(loadButton);
+    loadButton.id = 'click-me';
+    loadButton.innerHTML = 'click me!';
+    loadButton.addEventListener('click', () => {
+        addBarToQueue(loading);
+    });
 }
 
 function addBarToQueue(func) {
-	if (!_loading) {
-		_loading = true;
-		loadingQueue.push(func);
-		initiateLoading();
-	} else {
-		console.log('hi');
-		loadingQueue.push(func);
-	}
+    if (!_loading) {
+        _loading = true;
+        loadingQueue.push(func);
+        initiateLoading();
+    } else {
+        console.log('hi');
+        loadingQueue.push(func);
+    }
 }
 
 function initiateLoading() {
-	let func = loadingQueue.pop();
-	console.log(func);
-	let next = loadingQueue.pop();
-	func(next);
-	if (loadingQueue.length > 0) {
-		initiateLoading();
-	} else {
-		_loading = false;
-	}
-	console.log(loadingQueue);
+    let func = loadingQueue.pop();
+    console.log(_loading);
+    console.log('1', loadingQueue);
+    func();
+
+    if (loadingQueue.length > 0) {
+        initiateLoading();
+    } else {
+        _loading = false;
+    }
+    console.log('2', loadingQueue);
 }
 
-function loading(next) {
-	const loadingBar = document.createElement('DIV');
-	loadingBar.classList.add('loading-bar');
-	const loadingColor = document.createElement('DIV');
-	progressBarContainer.appendChild(loadingBar);
-	loadingBar.appendChild(loadingColor);
-	loadingColor.classList.add('loading-bar-color');
-	if (next !== undefined) {
-		setTimeout(() => {
-			next();
-		}, 3000);
-	}
-}
+const loading = () => {
+    const loadingBar = document.createElement('DIV');
+    loadingBar.classList.add('loading-bar');
+    const loadingColor = document.createElement('DIV');
+    progressBarContainer.appendChild(loadingBar);
+    loadingBar.appendChild(loadingColor);
+    loadingColor.classList.add('loading-bar-color');
+
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-	progressBarContainer = document.getElementById('progress-bar');
-	loadProgressBarsButton();
+    progressBarContainer = document.getElementById('progress-bar');
+    loadProgressBarsButton();
 });
